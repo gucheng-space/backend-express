975406fbfc535209ac4cb70d78388dfd4ed3b913
gucheng-space <gucheng25218@outlook.com>
2025-07-05 16:15:06 +0800

    导入TS和express

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("gretting")
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`🚀 服务已启动在 ${port} 端口`);
+});
+
+app.get("/", (request: Request, response: Response) => {
+  response.send("hello world");
+});
